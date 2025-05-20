2b220571dfdd4a67126510ed836dace0c3a2e7b0
xboxl <xboxl@DANIIL>
2025-05-20 23:45:57 +0300

    In progress

---
+++ FunctionsHomework1/FillRand.h
@@ -0,0 +1,5 @@
+#pragma once
+void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
+void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS);
--- FunctionsHomework1/main.cpp
+++ FunctionsHomework1/main.cpp
@@ -1,21 +1,9 @@
-#include<iostream>
-using namespace std;
-
-#define delimiter "\n---------------------------------------------------------------------\n"
-
-const int ROWS = 4;
-const int COLS = 5;
-
-template<typename T>
-void Sort(T arr[], const int n);
-template<typename T>
-void Sort(T arr[ROWS][COLS], const int ROWS, const int COLS);
-
-
-void FillRand(int arr[], const int n);
-void FillRand(double arr[], const int n);
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
-void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS);
+#include "stdafx.h"
+#include "Constants.h"
+#include "Sort.h"
+#include "FillRand.h"
+#include "Sort.cpp"
+#include "FillRand.cpp"
 
 template<typename T>
 void Print(T arr[], const int n);
@@ -354,47 +342,6 @@ void RightShift(T arr[ROWS][COLS], const int ROWS, const int COLS, int shifts)
 		LeftShift(arr[i], COLS, COLS - shifts);
 	}
 }
-void FillRand(int arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 100;
-	}
-
-}
-void FillRand(double arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 10000;
-		arr[i] /= 100;
-
-	}
-
-}
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			arr[i][j] = rand();
-		}
-	}
-
-}
-void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			arr[i][j] = rand() % 10000;
-			arr[i][j] /= 100;
-		}
-	}
-
-}
 template<typename T>
 void Print(T arr[], const int n)
 {
@@ -419,43 +366,5 @@ void Print(T arr[ROWS][COLS], const int ROWS, const int COLS)
 }
 
 
-template<typename T>
-void Sort(T arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n;j++)
-		{
-			if (arr[j] < arr[i])
-			{
-				int buffer = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buffer;
-			}
-		}
-	}
-}
-template<typename T>
-void Sort(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{	
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS;j++)
-		{
-			for (int k = i; k < ROWS; k++)
-			{
-				for (int l = k == i ? j + 1 : 0; l < COLS;l++)
-				{
-					if (arr[i][j] > arr[k][l])
-					{
-						T buffer = arr[i][j];
-						arr[i][j] = arr[k][l];
-						arr[k][l] = buffer;
-					}
 
-				}
-			}
-		}
-	}
-}
 
